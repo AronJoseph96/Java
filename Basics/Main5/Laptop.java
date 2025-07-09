eb748facc215e028dfb0037faf7f5e8bd1b0e166
VaderXI <aronjoseph8008@gmail.com>
2025-07-09 21:52:28 +0530

    Add files via upload

---
+++ Basics/Main5/Laptop.java
@@ -0,0 +1,3 @@
+public class Laptop{
+    public String brand = "Lenovo Ideapad";
+}
---
+++ Basics/Main5/Main5.java
@@ -0,0 +1,7 @@
+public class Main5{
+    public static void main(String args[]){
+        System.out.println("imca_6_Aron Joseph\n");
+        Laptop myLaptop = new Laptop();
+        System.out.println("Laptop Brand: " +myLaptop.brand);
+    }
+}

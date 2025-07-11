23c2d10ce7c826bfe5772e22a54f7f215490ac2c
VaderXI <aronjoseph8008@gmail.com>
2025-07-11 23:54:29 +0530

    Delete Basics/Main5/Main5.java

--- Basics/Main5/Main5.java
+++
@@ -1,7 +0,0 @@
-public class Main5{
-    public static void main(String args[]){
-        System.out.println("imca_6_Aron Joseph\n");
-        Laptop myLaptop = new Laptop();
-        System.out.println("Laptop Brand: " +myLaptop.brand);
-    }
-}
